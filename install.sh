685ffa9fd6960f8f6d20347f6ae6b8b788507a26
Swapnil Kulkarni <kulkarni.swapnil.v@gmail.com>
2023-03-22 22:35:31 +0530

    -Updated instruction

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 #replace vimrc 
 cp dot-files/vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Launch VIM and type \":PluginInstall\" ";
+echo "Launch VIM and type \":PluginInstall\" and ENTER";
